db5a0ef3d504f018161db5027f82efc8b7e5f0f5
Geoffrey Chandler <geoffc@gmail.com>
2017-01-10 14:03:44 -0800

    standard out, format request

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"log"
 	"time"
+	"fmt"
+	"strings"
 	"io/ioutil"
 )
 
@@ -30,9 +32,34 @@ func (logHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path == "/__health" {
 		w.WriteHeader(200)
 	} else {
+		fmt.Println(formatRequest(req))
 		body, _ := ioutil.ReadAll(req.Body)
-		log.Print("Request: ", req)
-		log.Print("Headers: ", req.Header)
-		log.Print("Body: ", string(body))
+		fmt.Println(string(body))
 	}
+}
+
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
 }
